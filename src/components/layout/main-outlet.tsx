3478c2bea704fd4a7f77fee2cb0a16df06dca10d
Arkar Soe <arkarsoedev@gmail.com>
2026-01-25 21:18:52 +0630

    fix: main layout shift

--- src/components/layout/main-outlet.tsx
+++ src/components/layout/main-outlet.tsx
@@ -2,20 +2,46 @@
 import { useSidebar } from "../ui/sidebar";
 import { cn } from "@/lib/utils";
 import { useIsMobile } from "@/hooks/use-mobile";
+import { useEffect, useState } from "react";
 
 const MainOutlet = ({ children }: { children?: React.ReactNode }) => {
+   const [isMounted, setIsMounted] = useState(false)
+
+   useEffect(() => {
+      setIsMounted(true)
+   })
+
    const { open } = useSidebar();
    const mobile = useIsMobile();
+
+   const getClasses = () => {
+      let classes = ""
+      if (mobile) {
+         classes = "max-w-full"
+      } else if (open) {
+         classes = "max-w-[calc(98vw-var(--sidebar-width))]"
+      } else {
+         classes = "max-w-[calc(92.9vw-var(--sidebar-width-icon)+(calc(0.3rem*4))+2px)]"
+      }
+      return classes
+   }
+
+   if (!isMounted) return <div
+      data-slot="sidebar-inset"
+      className={cn(
+         "mx-auto w-full max-w-full",
+         "px-4 pt-4 pb-4"
+      )}
+   >
+      {children}
+   </div>
 
    return (
       <div
          data-slot="sidebar-inset"
          className={cn(
             "mx-auto w-full",
-            open
-               ? "max-w-[calc(98vw-var(--sidebar-width))]"
-               : "max-w-[calc(92.9vw-var(--sidebar-width-icon)+(calc(0.3rem*4))+2px)]",
-            mobile && "max-w-full",
+            getClasses(),
             "px-4 pt-4 pb-4"
          )}
       >
