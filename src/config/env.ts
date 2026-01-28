31a42558397c33d50f81372a71a52f030ec6627d
Arkar Soe <arkarsoedev@gmail.com>
2026-01-28 15:25:25 +0630

    fix: env error

--- src/config/env.ts
+++ src/config/env.ts
@@ -16,7 +16,7 @@ const createEnv = () => {
    const envVars = {
       API_URL: process.env.NEXT_PUBLIC_API_URL,
       ENABLE_API_MOCKING: process.env.NEXT_PUBLIC_ENABLE_API_MOCKING,
-      APP_URL: process.env.NEXT_PUBLIC_URL,
+      APP_URL: process.env.NEXT_PUBLIC_APP_URL,
       APP_MOCK_API_PORT: process.env.NEXT_PUBLIC_MOCK_API_PORT,
    };
 
