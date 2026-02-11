9226782fa8508d93bf0811dda8c889d94f55c2f5
Arkar Soe <arkarsoedev@gmail.com>
2026-02-11 17:03:05 +0630

    testing hydration error on server

--- src/app/(dashboard)/(public)/page.tsx
+++ src/app/(dashboard)/(public)/page.tsx
@@ -3,7 +3,10 @@ export const revalidate = 60
 import HomePage from "@/features/home/pages";
 
 const HomePageRoute = () => {
-  return <HomePage />;
+  // return <HomePage />;
+  return <main>
+    homepage
+  </main>
 };
 
 export default HomePageRoute;
