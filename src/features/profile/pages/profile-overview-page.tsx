18db8ca307f8b94e0f64fb6c771e62540799f736
Arkar Soe <arkarsoedev@gmail.com>
2026-04-24 12:41:40 +0630

    style: resize featured images and improve grid responsiveness on overview pages

--- src/features/profile/pages/profile-overview-page.tsx
+++ src/features/profile/pages/profile-overview-page.tsx
@@ -161,7 +161,7 @@ const ProfileOverviewPage = () => {
                </Button>
             </div>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
                {featuredPhotos?.map((image) => (
                   <figure key={image.id} className="relative">
                      <img
